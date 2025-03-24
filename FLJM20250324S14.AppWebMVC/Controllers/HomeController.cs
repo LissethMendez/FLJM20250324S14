e0fb406e1c79f1339179ac8014c37f973305ac2d
Fátima Lisseth Jiménez Mendez <fatimalissjimenez@outlook.com>
2025-03-24 13:31:04 -0600

    Agregar archivos de proyecto.

---
+++ FLJM20250324S14.AppWebMVC/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using FLJM20250324S14.AppWebMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FLJM20250324S14.AppWebMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ FLJM20250324S14.AppWebMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FLJM20250324S14.AppWebMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
